ddd91dce890c35f929c55ae8d196291b54d361b7
akinlade muheebah <onikoyitinuade0526@gmail.com>
2024-06-15 11:11:29 +0100

    Update settings.py

--- IMS/settings.py
+++ IMS/settings.py
@@ -20,12 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 
@@ -153,10 +148,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
